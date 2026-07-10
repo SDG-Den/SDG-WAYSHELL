54fe61dced2d497a92b6a5e279805b70506035dc
SDGDen <sdgden@sdgcloud.nl>
2026-07-10 12:55:57 +0200

    auto-update

--- install.sh
+++ install.sh
@@ -5,28 +5,16 @@ unipkg install any waybar
 unipkg install any jq
 unipkg install any procps-ng
 
-# set working directory
-WORKDIR=$HOME/.cache/SDG-PKG/sdg-wayshell
+WORKDIR="$HOME/.cache/SDG-PKG/sdg-wayshell"
 
-# install default configs
-cp -r $WORKDIR/config/* $HOME/.config
+cp -r "$WORKDIR/config/"* "$HOME/.config/"
+cp -r "$WORKDIR/local/"* "$HOME/.local/"
+cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
+cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
 
-# install binaries
-cp -r $WORKDIR/local/* $HOME/.local
+chmod a+x "$HOME/.local/SDG-WAYSHELL/wayshell.sh"
+chmod a+x "$HOME/.local/SDG-WAYSHELL/modules/"*.sh
 
-# install docs and tips
-mkdir -p $HOME/.local/docs
-mkdir -p $HOME/.local/tips
-cp -r $WORKDIR/docs/* $HOME/.local/docs
-cp -r $WORKDIR/tips/* $HOME/.local/tips
+sudo ln -sf "$HOME/.local/SDG-WAYSHELL/wayshell.sh" /usr/bin/wayshell
 
-# make entrypoint executable
-chmod a+x $HOME/.local/SDG-WAYSHELL/wayshell.sh
-chmod a+x $HOME/.local/SDG-WAYSHELL/modules/*.sh
-
-# symlink entrypoint
-sudo ln -sf $HOME/.local/SDG-WAYSHELL/wayshell.sh /usr/bin/wayshell
-
-# verify binary
 which wayshell || echo "INSTALL FAILED!"
-
--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf $HOME/.local/SDG-WAYSHELL
-cp -r $HOME/.cache/SDG-PKG/sdg-wayshell/local/* $HOME/.local
-sudo ln -sf $HOME/.local/SDG-WAYSHELL/wayshell.sh /usr/bin/wayshell
+WORKDIR="$HOME/.cache/SDG-PKG/sdg-wayshell"
 
-rm -rf $HOME/.local/docs/SDG-WAYSHELL
-rm -rf $HOME/.local/tips/SDG-WAYSHELL
-mkdir -p $HOME/.local/docs
-mkdir -p $HOME/.local/tips
-cp -r $HOME/.cache/SDG-PKG/sdg-wayshell/docs/* $HOME/.local/docs
-cp -r $HOME/.cache/SDG-PKG/sdg-wayshell/tips/* $HOME/.local/tips
+rm -rf "$HOME/.local/SDG-WAYSHELL"
+cp -r "$WORKDIR/local/"* "$HOME/.local/"
+
+rm -rf "$HOME/.local/docs/SDG-WAYSHELL" "$HOME/.local/tips/SDG-WAYSHELL"
+cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
+cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
+
+sudo ln -sf "$HOME/.local/SDG-WAYSHELL/wayshell.sh" /usr/bin/wayshell
