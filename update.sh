dbee451e2c2f22c0ed5b13c8b2cd264d766e6316
SDGDen <sdgden@sdgcloud.nl>
2026-07-12 18:35:46 +0200

    quick push with sdgbuild

--- update.sh
+++ update.sh
@@ -5,6 +5,10 @@ WORKDIR="$HOME/.cache/SDG-PKG/sdg-wayshell"
 rm -rf "$HOME/.local/SDG-WAYSHELL"
 cp -r "$WORKDIR/local/"* "$HOME/.local/"
 
+rm "$HOME/.local/matugen/01-wayshell.toml"
+mkdir -p "$HOME/.local/matugen"
+cp -r "$WORKDIR/matugen/"* "$HOME/.local/matugen/"
+
 rm -rf "$HOME/.local/docs/SDG-WAYSHELL" "$HOME/.local/tips/SDG-WAYSHELL"
 cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
 cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
